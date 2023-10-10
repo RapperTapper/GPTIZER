a8733ec2f3185c0e72482980dbd448f7fc51ed16
RapperTapper <146704583+RapperTapper@users.noreply.github.com>
2023-10-10 13:19:45 +0200

    supabase.js aktualisiert

--- connection/supabase.js
+++ connection/supabase.js
@@ -1,8 +1,8 @@
 console.log("Initialisierung Supabase");
 
 // Supabase Initialisierung
-const supabaseUrl = ''
-const supabaseKey = ''
+const supabaseUrl = 'https://gpqouxszigofgdxszbuu.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwcW91eHN6aWdvZmdkeHN6YnV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY4NDQxNzYsImV4cCI6MjAxMjQyMDE3Nn0.a8uCxjG2p0zLn7MlSGFtodKa-6zbhzx8OXW3HO6bPNE'
 const supa = supabase.createClient(supabaseUrl, supabaseKey, {
     auth: {
         redirectTo: window.location.origin,  // This will redirect back to the page where the request originated from
