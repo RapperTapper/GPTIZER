a35cea9c9bae924160346a0a43e12d9313823d5b
RapperTapper <146704583+RapperTapper@users.noreply.github.com>
2023-10-11 10:01:10 +0200

    menu klickbar

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        function toggleMenu() {
+            var menu = document.getElementById("main-menu");
+            if (menu.style.display === "block") {
+            menu.style.display = "none";
+            } else {
+            menu.style.display = "block";
+            }
+        }
+    });
+</script>
