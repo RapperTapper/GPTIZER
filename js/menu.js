6a8094ede9c8aec48bdbbcb2ccb0500f0769f4c5
RapperTapper <raphael.schnell@stud.fhgr.ch>
2023-10-11 17:02:57 +0200

    menu closing when click somewhere on page

--- js/menu.js
+++ js/menu.js
@@ -6,3 +6,13 @@ function toggleMenu() {
         menu.style.display = "block";
     }
 }
+  
+function handleClick(event) {
+    var menu = document.getElementById("main-menu");
+    // check if the clicked element is not the menu button or the menu itself
+    if (!event.target.matches('.menu-button') && !event.target.closest('.menu') && menu.style.display === "block") {
+        toggleMenu();
+    }
+}
+
+document.addEventListener('click', handleClick);
