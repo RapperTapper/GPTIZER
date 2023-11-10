5dd177330fa969273634284178a5a849b1f87e19
RapperTapper <raphael.schnell@stud.fhgr.ch>
2023-11-10 03:04:12 +0100

    update for now

--- js/global-functions.js
+++ js/global-functions.js
@@ -1,5 +1,5 @@
 // Desc: Global functions for the site
-import { supa } from "./supabase.js";
+import { supa } from "../connection/supabase.js";
 
 export function redirect(url) {
     window.location.href = url;
@@ -11,7 +11,7 @@ export function checkNicknameCharacters() {
     // let pattern = /^[A-Za-z0-9-_.]+$/; // Do not allow empty string
     if (!pattern.test(nickname_input.value)) {
         alert('Invalid characters in nickname. Only A-Z, a-z, 0-9, -, _ and . are allowed.');
-        nickname_input.value = nickname_input.value.replace(/[^A-Za-z0-9-_.]/g, '_');
+        nickname_input.value = nickname_input.value.replace(/[^A-Za-z0-9-_.]/g, '');
     }
 }
 
